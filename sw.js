bf47790f4c47d3566c71c437cd6af3fb79d4bf4b
ToxicMinds <nikshanbhag@gmail.com>
2026-06-03 11:50:28 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,6 @@
-const CACHE_NAME = 'recomp-v2';
-const ASSETS = [
-  './body_recomposition_blueprint.html',
+const CACHE_NAME = 'recompos-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
   './manifest.json'
 ];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
